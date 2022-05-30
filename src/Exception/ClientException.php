8c1a1e553ca1356f6cd21466808958020e63cd35
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-30 17:14:13 -0400

    Create ClientException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/ClientException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Http\Exception;
+
+final class ClientException implements \Ghostwriter\Http\Contract\Exception\ClientExceptionInterface
+{
+
+}
