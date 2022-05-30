f89eeb201e5992635284721607f277045b5a126e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-30 17:12:27 -0400

    Create ResponseFactoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/Factory/ResponseFactoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Http\Contract\Factory;
+
+use Ghostwriter\Http\Contract\Message\ResponseInterface;
+
+interface ResponseFactoryInterface
+{
+    /**
+     * Create a new response.
+     *
+     * @param int    $code         HTTP status code; defaults to 200
+     * @param string $reasonPhrase reason phrase to associate with status code
+     *                             in generated response; if none is provided implementations MAY use
+     *                             the defaults as suggested in the HTTP specification
+     *
+     */
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface;
+}
