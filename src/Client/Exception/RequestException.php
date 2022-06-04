81d4c1a9c05acfd29a646c6d0fb1e04bc1b9969c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 14:18:35 -0400

    Update RequestException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Client/Exception/RequestException.php
+++ src/Client/Exception/RequestException.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Http\Client\Exception;
 
-use Ghostwriter\Http\Contract\Client\Exception\ClientExceptionInterface;
-use Ghostwriter\Http\Contract\Client\Exception\NetworkExceptionInterface;
 use Ghostwriter\Http\Contract\Client\Exception\RequestExceptionInterface;
 use Ghostwriter\Http\Contract\Message\RequestInterface;
 use RuntimeException;
-use Throwable;
 
 final class RequestException extends RuntimeException implements RequestExceptionInterface
 {
-
-    public function __construct(private RequestInterface $request, string $message)
-    {
+    public function __construct(
+        private RequestInterface $request,
+        string $message
+    ) {
         parent::__construct($message);
     }
 
