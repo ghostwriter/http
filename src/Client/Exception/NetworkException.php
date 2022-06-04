fdfc051809426c7c08cc31dbe89c40e092e6c896
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 13:54:21 -0400

    Create NetworkException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Client/Exception/NetworkException.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Http\Client\Exception;
+
+use Ghostwriter\Http\Contract\Client\Exception\ClientExceptionInterface;
+use Ghostwriter\Http\Contract\Client\Exception\NetworkExceptionInterface;
+use Ghostwriter\Http\Contract\Message\RequestInterface;
+use RuntimeException;
+use Throwable;
+
+final class NetworkException extends RuntimeException implements NetworkExceptionInterface
+{
+
+    public function __construct(private RequestInterface $request, string $message)
+    {
+        parent::__construct($message);
+    }
+
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+}
