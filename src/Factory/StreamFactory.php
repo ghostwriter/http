2189914ba47cd84033f0c5cf34b7242f0ff304f4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 14:49:38 -0400

    Update StreamFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/StreamFactory.php
+++ src/Factory/StreamFactory.php
@@ -6,23 +6,34 @@ namespace Ghostwriter\Http\Factory;
 
 use Ghostwriter\Http\Contract\Factory\StreamFactoryInterface;
 use Ghostwriter\Http\Contract\Message\StreamInterface;
+use Ghostwriter\Http\Message\Stream;
 use InvalidArgumentException;
-use RuntimeException;
+use function fopen;
+use function fwrite;
+use function rewind;
 
 final class StreamFactory implements StreamFactoryInterface
 {
     public function createStream(string $content = ''): StreamInterface
     {
-        throw new RuntimeException();
+        $resource = fopen('php://temp', 'rb+');
+        fwrite($resource, $content);
+        rewind($resource);
+
+        return $this->createStreamFromResource($resource);
     }
 
-    public function createStreamFromFile(string $filename, string $mode = 'r'): StreamInterface
+    public function createStreamFromFile(string $filename, string $mode = 'rb+'): StreamInterface
     {
-        throw new InvalidArgumentException();
+        return new Stream($filename, $mode);
     }
 
     public function createStreamFromResource($resource): StreamInterface
     {
-        throw new InvalidArgumentException();
+        if (! is_resource($resource) || 'stream' !== get_resource_type($resource)) {
+            throw new InvalidArgumentException('Invalid stream resource provided');
+        }
+
+        return Stream::create($resource);
     }
 }
