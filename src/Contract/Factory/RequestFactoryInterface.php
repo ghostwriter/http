f4b9710d4b1fc19812968cc97a20c0f67a825149
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 13:57:12 -0400

    Update RequestFactoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/Factory/RequestFactoryInterface.php
+++ src/Contract/Factory/RequestFactoryInterface.php
@@ -12,11 +12,10 @@ interface RequestFactoryInterface
     /**
      * Create a new request.
      *
-     * @param string              $method the HTTP method associated with the request
-     * @param string|UriInterface $uri    The URI associated with the request. If
-     *                                    the value is a string, the factory MUST create a UriInterface
-     *                                    instance based on it.
+     * @param string              $method The HTTP method associated with the request
+     * @param string|UriInterface $uri    The URI associated with the request. If the value is a string,
+     *                                    the factory MUST create a UriInterface instance based on it.
      *
      */
-    public function createRequest(string $method, string|UriInterface $uri): RequestInterface;
+    public function createRequest(string $method, UriInterface|string $uri): RequestInterface;
 }
