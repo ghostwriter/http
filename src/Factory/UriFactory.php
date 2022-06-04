8b325c0cec30c9203d3d1d1db211f211a212d111
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 14:53:53 -0400

    Update UriFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/UriFactory.php
+++ src/Factory/UriFactory.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace Ghostwriter\Http\Factory;
 
 use Ghostwriter\Http\Contract\Factory\UriFactoryInterface;
-use Ghostwriter\Http\Contract\Factory\UriInterface;
-use InvalidArgumentException;
+use Ghostwriter\Http\Contract\Message\UriInterface;
+use Ghostwriter\Http\Message\Uri;
 
 final class UriFactory implements UriFactoryInterface
 {
     public function createUri(string $uri = ''): UriInterface
     {
-        throw new InvalidArgumentException();
+        return new Uri($uri);
     }
 }
