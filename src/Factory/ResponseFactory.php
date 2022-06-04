c73b595755ace5a02fac512e3d18785331529020
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 14:18:54 -0400

    Update ResponseFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/ResponseFactory.php
+++ src/Factory/ResponseFactory.php
@@ -6,12 +6,12 @@ namespace Ghostwriter\Http\Factory;
 
 use Ghostwriter\Http\Contract\Factory\ResponseFactoryInterface;
 use Ghostwriter\Http\Contract\Message\ResponseInterface;
-use RuntimeException;
+use Ghostwriter\Http\Message\Response;
 
 final class ResponseFactory implements ResponseFactoryInterface
 {
     public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
     {
-        throw new RuntimeException();
+        return (new Response())->withStatus($code, $reasonPhrase);
     }
 }
