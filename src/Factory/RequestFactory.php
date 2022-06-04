50319de4cfd6a36c1427c42ae20891f969e8e3e4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-04 14:18:43 -0400

    Update RequestFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/RequestFactory.php
+++ src/Factory/RequestFactory.php
@@ -7,12 +7,12 @@ namespace Ghostwriter\Http\Factory;
 use Ghostwriter\Http\Contract\Factory\RequestFactoryInterface;
 use Ghostwriter\Http\Contract\Message\RequestInterface;
 use Ghostwriter\Http\Contract\Message\UriInterface;
-use RuntimeException;
+use Ghostwriter\Http\Message\Request;
 
 final class RequestFactory implements RequestFactoryInterface
 {
     public function createRequest(string $method, UriInterface|string $uri): RequestInterface
     {
-        throw new RuntimeException();
+        return new Request($method, $uri);
     }
 }
