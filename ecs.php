104db932ff4d353b98c2831bafd1b74bd31ebc81
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-08 12:58:27 -0500

    Update ecs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- ecs.php
+++ ecs.php
@@ -72,7 +72,6 @@ use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->parallel();
     $ecsConfig->paths([
-        __DIR__ . '/compliance.php',
         __DIR__ . '/ecs.php',
         __DIR__ . '/rector.php',
         __DIR__ . '/src',
