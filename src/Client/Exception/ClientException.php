8f206f4968c68bb9ea99daf405df633324937e07
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-11-26 08:17:15 -0600

    Update ClientException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Client/Exception/ClientException.php
+++ src/Client/Exception/ClientException.php
@@ -9,4 +9,18 @@ use RuntimeException;
 
 final class ClientException extends RuntimeException implements ClientExceptionInterface
 {
+    public static function unableToConfigureCurlHandle(): self
+    {
+        return new self('Unable to configure CurlHandle');
+    }
+
+    public static function unableToCreateCurlHandle(): self
+    {
+        return new self('Unable to create CurlHandle');
+    }
+
+    public static function unableToCreateCurlMultiHandle(): self
+    {
+        return new self('Unable to create CurlMultiHandle');
+    }
 }
