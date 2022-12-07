d10374f3aa00ebc34130c52a13d2d53fd6d86462
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-07 12:12:50 -0600

    Update NetworkException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Client/Exception/NetworkException.php
+++ src/Client/Exception/NetworkException.php
@@ -15,9 +15,9 @@ final class NetworkException extends RuntimeException implements NetworkExceptio
         private RequestInterface $request,
         string $message,
         int $code = 0,
-        Throwable $previous = null
+        Throwable $throwable = null
     ) {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code, $throwable);
     }
 
     public function getRequest(): RequestInterface
