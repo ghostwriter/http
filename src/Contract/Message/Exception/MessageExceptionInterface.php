cf8a7bf67b1c79168b726d898fbef854bcbdf164
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-10 19:55:15 -0600

    Create MessageExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/Message/Exception/MessageExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Http\Contract\Message\Exception;
+
+use Throwable;
+
+/**
+ * Http Message related exception.
+ */
+interface MessageExceptionInterface extends Throwable
+{
+}
