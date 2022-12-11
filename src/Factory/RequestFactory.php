7f1cc0a55c7b153899496a77f21807da59d24266
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-10 19:52:16 -0600

    Update RequestFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/RequestFactory.php
+++ src/Factory/RequestFactory.php
@@ -8,11 +8,12 @@ use Ghostwriter\Http\Contract\Factory\RequestFactoryInterface;
 use Ghostwriter\Http\Contract\Message\RequestInterface;
 use Ghostwriter\Http\Contract\Message\UriInterface;
 use Ghostwriter\Http\Message\Request;
+use Ghostwriter\Http\Message\Uri;
 
 final class RequestFactory implements RequestFactoryInterface
 {
     public function createRequest(string $method, UriInterface|string $uri): RequestInterface
     {
-        return new Request($method, $uri);
+        return new Request($method, $uri instanceof UriInterface ? $uri : new Uri($uri));
     }
 }
