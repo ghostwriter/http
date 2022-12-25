d38fdb26c81e95661721fe59bfff7c01f63c1684
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-25 12:51:09 -0600

    Update Cookie.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Message/Cookie.php
+++ src/Message/Cookie.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Http\Message;
 
+use Stringable;
+
 final class Cookie implements Stringable
 {
+    public function __toString(): string
+    {
+        return 'NOT IMPLEMENTED!';
+    }
 }
