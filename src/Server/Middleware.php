40525e0d0ccd85d3200714d5a4e434bfcce35412
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-12-25 12:50:46 -0600

    Update Middleware.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Server/Middleware.php
+++ src/Server/Middleware.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Http\Server;
 
+use Ghostwriter\Http\Contract\Message\ResponseInterface;
+use Ghostwriter\Http\Contract\Message\ServerRequestInterface;
+use Ghostwriter\Http\Contract\Server\RequestHandlerInterface;
+
 final class Middleware extends AbstractMiddleware
 {
+    public function process(
+        ServerRequestInterface $serverRequest,
+        RequestHandlerInterface $requestHandler
+    ): ResponseInterface {
+        return $requestHandler->handle($serverRequest);
+    }
 }
