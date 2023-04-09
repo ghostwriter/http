68351b08d333f360c8ef4454f8f583b596d58db1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-09 12:51:58 -0500

    Create Parser.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Analysis/Parser.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Http\Analysis;
+
+use Generator;
+
+/** @see ParserTest */
+final class Parser
+{
+    public function __construct(
+        private readonly Lexer $lexer
+    ) {
+    }
+
+    /**
+     * @return Generator<Node|Token>
+     */
+    public function parse(string $document): Generator
+    {
+        yield from $this->lexer->lex($document);
+    }
+}
